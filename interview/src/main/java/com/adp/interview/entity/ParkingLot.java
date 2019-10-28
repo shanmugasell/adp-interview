9b0854edffc539c20a60dc9b9e91ee3ed6a15e3b
Shanmuga <shanmugasell@gmail.com>
2019-10-28 08:37:02 -0400

    h2 db and save parking lots changes

--- interview/src/main/java/com/adp/interview/entity/ParkingLot.java
+++ interview/src/main/java/com/adp/interview/entity/ParkingLot.java
@@ -1,19 +1,34 @@
 package com.adp.interview.entity;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Data
+@Entity
 @Component
-public class ParkingLot {
+public class ParkingLot implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
 	public long id;
 	
+	@Column
 	public String number;
 	
+	@Column
 	public String type;
 	
+	@Column
 	public double rate = 2.00;
 	
 }
